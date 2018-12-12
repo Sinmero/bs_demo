ca280296c6a1811adc88155dc8546550370f3742
Sinm <38570414+Sinmero@users.noreply.github.com>
2018-12-12 22:30:13 +0300

    V.0.9

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 $('document').ready(function () {
 
-    // window.location.href = "lc";
-    
+
     $('.side-navbar .dropright').hover(function () {
 
         $(this).find('.dropdown-menu').stop().slideDown(350)
@@ -9,6 +8,32 @@ $('document').ready(function () {
     },function () {
 
         $(this).find('.dropdown-menu').stop().slideUp(50)
+
+    });
+
+    $('.more-categories').click(function () {
+
+        $('.show-more').addClass('d-block').removeClass('d-none');
+
+        $('.more-categories').css('display', 'none')
+
+    });
+
+    $('.navbar .input-group .btn').hover(function () {
+
+        $('.navbar .input-group .form-control').focus().stop()
+            .css('backgroundColor', 'rgba(255,255,255,1.0)')
+            .css('opacity', '1.0')
+            .animate({width: '1%'}, 200);
+
+    });
+
+    $('.navbar .input-group .form-control').focusout(function () {
+        
+        $('.navbar .input-group .form-control').stop()
+            .css('backgroundColor', 'rgba(255,255,255,1.0)')
+            .css('opacity', '0.0')
+            .animate({width: '0'}, 200)
 
     })
 
